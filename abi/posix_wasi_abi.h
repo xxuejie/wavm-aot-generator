1aa5199536e628425c7958c24fcf1ea77e2b1bf0
Xuejie Xiao <xxuejie@gmail.com>
2020-02-27 01:25:49 +0000

    Add POSIX WASI integration ABI

---
+++ abi/posix_wasi_abi.h
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+#ifndef WAVM_POSIX_WASI_ABI_H
+#define WAVM_POSIX_WASI_ABI_H
+
+#ifndef MEMORY0_DEFINED
+extern uint8_t* memory0;
+#endif
+
+int32_t wavm_wasi_unstable_fd_write(void* dummy, int32_t fd, int32_t address, int32_t num, int32_t written_bytes_address)
+{
+  static uint8_t temp_buffer[65];
+
+  int32_t written_bytes = 0;
+  for (int32_t i = 0; i < num; i++) {
+    uint32_t buffer_address = *((uint32_t*) &memory0[address + i * 8]);
+    uint8_t* buf = &memory0[buffer_address];
+    uint32_t buffer_length = *((uint32_t*) &memory0[address + i * 8 + 4]);
+
+    int32_t written = write(fd, buf, buffer_length);
+    written_bytes += buffer_length;
+  }
+  if (written_bytes_address != 0) {
+    *((uint32_t*) &memory0[written_bytes_address]) = written_bytes;
+  }
+
+  return 0;
+}
+
+void wavm_wasi_unstable_proc_exit(void* dummy, int32_t code)
+{
+  exit(code);
+}
+
+#endif  /* WAVM_POSIX_WASI_ABI_H */
